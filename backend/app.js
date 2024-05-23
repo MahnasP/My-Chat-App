34e9316b41db29bd09ef80f0a50f6c0fd3c4ccca
MahnasP <mahnasmailbox@gmail.com>
2024-05-23 14:58:25 +0530

    test deploy

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import authRoutes from "./src/routes/auth.routes.js"
 import messageRoutes from "./src/routes/message.routes.js"
@@ -5,7 +6,7 @@ import userRoutes from "./src/routes/user.routes.js"
 import cookieParser from "cookie-parser";
 import { app } from "./src/socket/socket.js";
 
-
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,5 +16,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 export { app };
