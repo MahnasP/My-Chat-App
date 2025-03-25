e9469b21bcba0e8169edf1ddd6fad3ec83964b74
Mahnas P <mahnasmailbox@gmail.com>
2025-03-25 22:45:25 +0530

    Add CORS

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -3,6 +3,8 @@ import http from "http";
 import express from "express";
 import dotenv from "dotenv";
 import { Message } from "../models/message.model.js";
+import cors from "cors";
+
 dotenv.config({
   path: "./.env",
 });
@@ -15,6 +17,14 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+
+app.use(
+  cors({
+    origin: process.env.FRONT_END_ORIGIN || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 const userSocketMap = {}; //{userId:socketId}
 
@@ -32,7 +42,6 @@ io.on("connection", (socket) => {
   //to mark messages as seen
   socket.on("markMessagesAsSeen", async ({ currUserID, otherUserId }) => {
     try {
-      
       await Message.updateMany(
         { senderId: otherUserId, receiverId: currUserID, seen: false },
         { $set: { seen: true } }
